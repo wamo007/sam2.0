c69276b35a58ea41516cefb1a180b3841a6cd705
wamo007 <shamil010@hotmail.com>
2025-05-16 22:32:17 +0200

    Update Dimensions.ts

--- configs/Dimensions.ts
+++ configs/Dimensions.ts
@@ -1,19 +1,18 @@
+import { useState } from 'react';
 import { Dimensions } from 'react-native';
 import { scale as reactNativeScale } from 'react-native-size-matters';
 
-const isTablet = Dimensions.get('window').width > Dimensions.get('window').height; // Common tablet breakpoint
-const isSmallPhone = Dimensions.get('window').height <= 768;
-
 export const scale = (size: number): number => {
-  if (isTablet) {
-    return reactNativeScale(size * 0.6);
+  if (Dimensions.get('window').width > Dimensions.get('window').height) {
+    return reactNativeScale(size * 0.5);
   }
-  if (isSmallPhone) {
-    // Reduce scaling factor for tablets
+  if (Dimensions.get('window').height <= 768) {
     return reactNativeScale(size * 0.9);
   }
   return reactNativeScale(size);
 };
 
-export const isTabletDevice = isTablet;
-export const isSmallDevice = isSmallPhone;
+export const isTabletDevice = Dimensions.get('window').width > Dimensions.get('window').height;
+export const isSmallDevice = Dimensions.get('window').height <= 768 && Dimensions.get('window').width <= Dimensions.get('window').height;
+console.log(isSmallDevice)
+console.log(isTabletDevice)
